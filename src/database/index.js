fae08010381e7896fa8c389f3f829e91bff1217c
Đại Lộc Lê Quang <dailoc.lq@gmail.com>
2023-03-22 12:42:26 +0700

    debugging

--- src/database/index.js
+++ src/database/index.js
@@ -4,4 +4,6 @@ const knexFile = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const options = knexFile[env];
 
+console.log('options', options)
+
 module.exports = Knex(options)
